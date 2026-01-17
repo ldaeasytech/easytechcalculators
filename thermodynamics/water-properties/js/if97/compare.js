ec138ac8aa1de92c8380ab508ed07f9199b5cc02
ldaeasytech <lda.easytech@gmail.com>
2026-01-18 06:43:24 +0800

    Update compare.js

--- thermodynamics/water-properties/js/if97/compare.js
+++ thermodynamics/water-properties/js/if97/compare.js
@@ -1,1 +1,22 @@
+import { computeIF97 } from "./if97.js";
+
+export function compareToIF97(T, P, props) {
+  const ref = computeIF97(T, P);
+  const comparison = {};
+
+  for (const key in props) {
+    if (typeof props[key] === "number" && typeof ref[key] === "number") {
+      const err = props[key] - ref[key];
+      const rel = err / ref[key] * 100;
+      comparison[key] = {
+        model: props[key],
+        IF97: ref[key],
+        absError: err,
+        relErrorPercent: rel
+      };
+    }
+  }
+
+  return comparison;
+}
 
