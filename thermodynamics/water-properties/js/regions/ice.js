605e2c2ce98d8aa343c5956d3a09da82b87b75a9
ldaeasytech <lda.easytech@gmail.com>
2026-01-18 06:44:15 +0800

    Update ice.js

--- thermodynamics/water-properties/js/regions/ice.js
+++ thermodynamics/water-properties/js/regions/ice.js
@@ -1,1 +1,13 @@
+export function compute(T, P) {
+  return {
+    density: 917,
+    specificVolume: 1 / 917,
+    enthalpy: 2.05 * (T - 273.15),
+    entropy: (2.05 * (T - 273.15)) / T,
+    cp: 2.05,
+    cv: 1.8,
+    viscosity: 1e12,
+    conductivity: 2.2
+  };
+}
 
