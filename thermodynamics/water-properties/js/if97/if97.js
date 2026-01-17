8d9d1512035d8265a61f6640477075d69a87c1dc
ldaeasytech <lda.easytech@gmail.com>
2026-01-18 06:43:09 +0800

    Update if97.js

--- thermodynamics/water-properties/js/if97/if97.js
+++ thermodynamics/water-properties/js/if97/if97.js
@@ -1,1 +1,14 @@
+import { props as r1 } from "./region1.js";
+import { props as r2 } from "./region2.js";
+import { props as r3 } from "./region3.js";
+import { props as r5 } from "./region5.js";
+import { Tsat } from "./region4.js";
+
+export function computeIF97(T, P) {
+  const Ts = Tsat(P);
+  if (T < Ts && P > 1e5) return r1(T, P);
+  if (T > Ts && T < 1073.15) return r2(T, P);
+  if (T >= 1073.15) return r5(T, P);
+  return r3(T, P);
+}
 
