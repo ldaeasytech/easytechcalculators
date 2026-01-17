c918ce0044c5f796b6fe606617ad4f5ed179d71c
ldaeasytech <lda.easytech@gmail.com>
2026-01-18 06:43:54 +0800

    Update liquid.js

--- thermodynamics/water-properties/js/regions/liquid.js
+++ thermodynamics/water-properties/js/regions/liquid.js
@@ -1,1 +1,25 @@
+export function compute(T, P) {
+  return {
+    density: densityLiquid(T),
+    specificVolume: 1 / densityLiquid(T),
+    enthalpy: enthalpyLiquid(T),
+    entropy: entropyLiquid(T),
+    cp: 4.18,
+    cv: 3.12,
+    viscosity: 1e-3,
+    conductivity: 0.6
+  };
+}
+
+export function densityLiquid(T) {
+  return 1000 / (1 + 0.0003 * (T - 273.15));
+}
+
+export function enthalpyLiquid(T) {
+  return 4.18 * (T - 273.15);
+}
+
+export function entropyLiquid(T) {
+  return enthalpyLiquid(T) / T;
+}
 
