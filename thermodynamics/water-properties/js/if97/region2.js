748eaf1645937b5dc458e07c753726b882ccbc9b
ldaeasytech <lda.easytech@gmail.com>
2026-01-18 06:41:58 +0800

    Update region2.js

--- thermodynamics/water-properties/js/if97/region2.js
+++ thermodynamics/water-properties/js/if97/region2.js
@@ -1,1 +1,9 @@
+export function props(T, P) {
+  const rho = P / (461.5 * T);
+  const h = 2500 + 1.9 * (T - 373.15);
+  const s = h / T;
+  const cp = 2.08;
+  const cv = 1.54;
+  return { density: rho, enthalpy: h, entropy: s, cp, cv };
+}
 
