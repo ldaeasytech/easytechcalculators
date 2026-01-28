b38c12462cf69d7c783807718fc2c520c5e2eadd
ldaeasytech <lda.easytech@gmail.com>
2026-01-28 14:22:01 +0800

    Update pressure.js

--- docs/water-properties/js/iapws95/pressure.js
+++ docs/water-properties/js/iapws95/pressure.js
@@ -1,3 +1,16 @@
-pressure(T, rho)
-dPdrho(T, rho)
+import { R, Tc, rhoc } from "./constants95.js";
+import { helmholtz } from "./derivatives.js";
 
+// Pressure in MPa
+export function pressure(T, rho) {
+  const h = helmholtz(T, rho, Tc, rhoc);
+  return rho * R * T * (1 + h.delta * h.ar_d) * 1e-3;
+}
+
+export function dPdrho(T, rho) {
+  const h = helmholtz(T, rho, Tc, rhoc);
+  const term =
+    1 + 2 * h.delta * h.ar_d + h.delta * h.delta * h.ar_dd;
+
+  return R * T * term * 1e-3;
+}
