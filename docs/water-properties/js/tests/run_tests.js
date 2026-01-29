2aa2d719de6221f304c0464169bee0d7352c6e36
ldaeasytech <lda.easytech@gmail.com>
2026-01-29 09:34:06 +0800

    Create run_tests.js

---
+++ docs/water-properties/js/tests/run_tests.js
@@ -0,0 +1,6 @@
+console.log("🔥 Water Properties Test Runner Loaded");
+
+// Import tests (side effects = execution)
+import "./test_tp_singlephase.js";
+import "./test_ph_singlephase.js";
+import "./test_ps_singlephase.js";
