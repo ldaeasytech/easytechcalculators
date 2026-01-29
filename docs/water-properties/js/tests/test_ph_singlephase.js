13b3c01902da83700c0d7203c955f0c441f3b354
ldaeasytech <lda.easytech@gmail.com>
2026-01-29 09:42:55 +0800

    Update test_ph_singlephase.js

--- docs/water-properties/js/tests/test_ph_singlephase.js
+++ docs/water-properties/js/tests/test_ph_singlephase.js
@@ -11,10 +11,10 @@ const TOL_T = 1e-5;
 const TOL_H = 5e-4;
 
 const seedTP = [
-  { T: 320, P: 8e6 },
-  { T: 380, P: 15e6 },
-  { T: 650, P: 1e6 },
-  { T: 850, P: 3e6 }
+  { T: 320, P: 8 },
+  { T: 380, P: 15 },
+  { T: 650, P: 1 },
+  { T: 850, P: 3 }
 ];
 
 console.log("=== PH SINGLE-PHASE TEST ===");
