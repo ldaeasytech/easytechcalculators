60ea79696268016efb385343ce356fec2e1e544b
ldaeasytech <lda.easytech@gmail.com>
2026-02-16 22:28:19 +0800

    Create pumpEfficiencyData.js

---
+++ docs/pump-power-calculator/js/data/pumpEfficiencyData.js
@@ -0,0 +1,11 @@
+// pumpEfficiencyData.js
+
+export const PUMP_TYPES = {
+  "Centrifugal (small)": { min: 0.50, max: 0.70 },
+  "Centrifugal (large)": { min: 0.70, max: 0.90 },
+  "Axial flow": { min: 0.75, max: 0.90 },
+  "Mixed flow": { min: 0.65, max: 0.85 },
+  "Gear pump": { min: 0.70, max: 0.85 },
+  "Piston pump": { min: 0.80, max: 0.95 },
+  "Screw pump": { min: 0.65, max: 0.85 }
+};
