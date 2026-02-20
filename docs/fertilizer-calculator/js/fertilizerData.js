e3aafae85e1344e6fed39cec28588157c0481083
ldaeasytech <lda.easytech@gmail.com>
2026-02-20 10:55:41 +0800

    Create fertilizerData.js

---
+++ docs/fertilizer-calculator/js/fertilizerData.js
@@ -0,0 +1,14 @@
+// fertilizerData.js
+
+export const fertilizers = {
+  A: { name: "Diammonium Phosphate", N: 9,  P: 23, K: 0 },
+  B: { name: "Ammonium Phosphate",    N: 8,  P: 10, K: 0 },
+  C: { name: "Muriate of Potash",    N: 0,  P: 0,  K: 30 },
+  D: { name: "Ammonium Sulfate",     N: 10.5, P: 0, K: 0 },
+  E: { name: "Ammonium Chloride",    N: 12.5, P: 0, K: 0 },
+  F: { name: "Superphosphate",       N: 0,  P: 9,  K: 0 },
+  G: { name: "Calcium Ammonium Nitrate", N: 13.5, P: 0, K: 0 },
+  H: { name: "Sulfate of Potash",    N: 0,  P: 0,  K: 26 },
+  I: { name: "Urea",                 N: 23, P: 0,  K: 0 },
+  J: { name: "Complete (Triple 14)", N: 7,  P: 7,  K: 7 }
+};
