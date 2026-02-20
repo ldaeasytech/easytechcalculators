c17db2cbb9a665b216c09f377307625c67b624b5
ldaeasytech <lda.easytech@gmail.com>
2026-02-20 22:42:05 +0800

    Update fertilizerData.js

--- docs/fertilizer-calculator/js/fertilizerData.js
+++ docs/fertilizer-calculator/js/fertilizerData.js
@@ -4,69 +4,69 @@ export const fertilizers = {
     short: "DAP",
     grade: "18-46-0",
     display: "Diammonium Phosphate (DAP, 18-46-0)",
-    N: 9, P: 23, K: 0
+    N: 18, P: 46, K: 0
   },
   B: {
     name: "Monoammonium Phosphate",
     short: "MAP",
     grade: "11-52-0",
     display: "Monoammonium Phosphate (MAP, 11-52-0)",
-    N: 8, P: 10, K: 0
+    N: 11, P: 52, K: 0
   },
   C: {
     name: "Muriate of Potash",
     short: "MOP",
     grade: "0-0-60",
     display: "Muriate of Potash (MOP, 0-0-60)",
-    N: 0, P: 0, K: 30
+    N: 0, P: 0, K: 60
   },
   D: {
     name: "Ammonium Sulfate",
     short: "",
     grade: "21-0-0",
     display: "Ammonium Sulfate (21-0-0)",
-    N: 10.5, P: 0, K: 0
+    N: 21, P: 0, K: 0
   },
   E: {
     name: "Ammonium Chloride",
     short: "",
     grade: "25-0-0",
     display: "Ammonium Chloride (25-0-0)",
-    N: 12.5, P: 0, K: 0
+    N: 25, P: 0, K: 0
   },
   F: {
     name: "Single Superphosphate",
     short: "SSP",
     grade: "0-18-0",
     display: "Single Superphosphate (SSP, 0-18-0)",
-    N: 0, P: 9, K: 0
+    N: 0, P: 18, K: 0
   },
   G: {
     name: "Calcium Ammonium Nitrate",
     short: "CAN",
     grade: "27-0-0",
     display: "Calcium Ammonium Nitrate (CAN, 27-0-0)",
-    N: 13.5, P: 0, K: 0
+    N: 27, P: 0, K: 0
   },
   H: {
     name: "Sulfate of Potash",
     short: "SOP",
     grade: "0-0-50",
     display: "Sulfate of Potash (SOP, 0-0-50)",
-    N: 0, P: 0, K: 26
+    N: 0, P: 0, K: 50
   },
   I: {
     name: "Urea",
     short: "",
     grade: "46-0-0",
     display: "Urea (46-0-0)",
-    N: 23, P: 0, K: 0
+    N: 46, P: 0, K: 0
   },
   J: {
     name: "NPK 14-14-14",
     short: "",
     grade: "14-14-14",
     display: "Complete Fertilizer (14-14-14)",
-    N: 7, P: 7, K: 7
+    N: 14, P: 14, K: 14
   }
 };
