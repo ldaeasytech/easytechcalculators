3236485d946d570da171678530307624881a04de
ldaeasytech <lda.easytech@gmail.com>
2026-02-20 15:07:46 +0800

    Update fertilizerData.js

--- docs/fertilizer-calculator/js/fertilizerData.js
+++ docs/fertilizer-calculator/js/fertilizerData.js
@@ -1,14 +1,72 @@
-// fertilizerData.js
-
 export const fertilizers = {
-  A: { name: "Diammonium Phosphate", N: 9,  P: 23, K: 0 },
-  B: { name: "Ammonium Phosphate",    N: 8,  P: 10, K: 0 },
-  C: { name: "Muriate of Potash",    N: 0,  P: 0,  K: 30 },
-  D: { name: "Ammonium Sulfate",     N: 10.5, P: 0, K: 0 },
-  E: { name: "Ammonium Chloride",    N: 12.5, P: 0, K: 0 },
-  F: { name: "Superphosphate",       N: 0,  P: 9,  K: 0 },
-  G: { name: "Calcium Ammonium Nitrate", N: 13.5, P: 0, K: 0 },
-  H: { name: "Sulfate of Potash",    N: 0,  P: 0,  K: 26 },
-  I: { name: "Urea",                 N: 23, P: 0,  K: 0 },
-  J: { name: "Complete (Triple 14)", N: 7,  P: 7,  K: 7 }
+  A: {
+    name: "Diammonium Phosphate",
+    short: "DAP",
+    grade: "18-46-0",
+    display: "Diammonium Phosphate (DAP, 18-46-0)",
+    N: 9, P: 23, K: 0
+  },
+  B: {
+    name: "Monoammonium Phosphate",
+    short: "MAP",
+    grade: "11-52-0",
+    display: "Monoammonium Phosphate (MAP, 11-52-0)",
+    N: 8, P: 10, K: 0
+  },
+  C: {
+    name: "Muriate of Potash",
+    short: "MOP",
+    grade: "0-0-60",
+    display: "Muriate of Potash (MOP, 0-0-60)",
+    N: 0, P: 0, K: 30
+  },
+  D: {
+    name: "Ammonium Sulfate",
+    short: "",
+    grade: "21-0-0",
+    display: "Ammonium Sulfate (21-0-0)",
+    N: 10.5, P: 0, K: 0
+  },
+  E: {
+    name: "Ammonium Chloride",
+    short: "",
+    grade: "25-0-0",
+    display: "Ammonium Chloride (25-0-0)",
+    N: 12.5, P: 0, K: 0
+  },
+  F: {
+    name: "Single Superphosphate",
+    short: "SSP",
+    grade: "0-18-0",
+    display: "Single Superphosphate (SSP, 0-18-0)",
+    N: 0, P: 9, K: 0
+  },
+  G: {
+    name: "Calcium Ammonium Nitrate",
+    short: "CAN",
+    grade: "27-0-0",
+    display: "Calcium Ammonium Nitrate (CAN, 27-0-0)",
+    N: 13.5, P: 0, K: 0
+  },
+  H: {
+    name: "Sulfate of Potash",
+    short: "SOP",
+    grade: "0-0-50",
+    display: "Sulfate of Potash (SOP, 0-0-50)",
+    N: 0, P: 0, K: 26
+  },
+  I: {
+    name: "Urea",
+    short: "",
+    grade: "46-0-0",
+    display: "Urea (46-0-0)",
+    N: 23, P: 0, K: 0
+  },
+  J: {
+    name: "NPK 14-14-14",
+    short: "",
+    grade: "14-14-14",
+    display: "Complete Fertilizer (14-14-14)",
+    N: 7, P: 7, K: 7
+  }
 };
