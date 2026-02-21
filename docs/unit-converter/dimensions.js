0317c31403d3f48bc540e06b97b209672e73752f
ldaeasytech <lda.easytech@gmail.com>
2026-02-22 07:01:47 +0800

    Create dimensions.js

---
+++ docs/unit-converter/dimensions.js
@@ -0,0 +1,24 @@
+export const dimensions = {
+
+  mass: "[M]",
+  length: "[L]",
+  time: "[T]",
+  temperature: "[Θ]",
+  area: "[L²]",
+  velocity: "[L T⁻¹]",
+  force: "[M L T⁻²]",
+  pressure: "[M L⁻¹ T⁻²]",
+  energy: "[M L² T⁻²]",
+  power: "[M L² T⁻³]",
+  momentum: "[M L T⁻¹]",
+  torque: "[M L² T⁻²]",
+  density: "[M L⁻³]",
+  viscosityDynamic: "[M L⁻¹ T⁻¹]",
+  viscosityKinematic: "[L² T⁻¹]",
+  flowRateVol: "[L³ T⁻¹]",
+  flowRateMass: "[M T⁻¹]",
+  thermalConductivity: "[M L T⁻³ Θ⁻¹]",
+  heatTransferCoeff: "[M T⁻³ Θ⁻¹]",
+  specificHeat: "[L² T⁻² Θ⁻¹]"
+
+};
