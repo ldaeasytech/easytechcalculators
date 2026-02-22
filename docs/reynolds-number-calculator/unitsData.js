41682834436acbd5e5289e52cb906b837fb273b8
ldaeasytech <lda.easytech@gmail.com>
2026-02-23 06:27:19 +0800

    Create unitsData.js

---
+++ docs/reynolds-number-calculator/unitsData.js
@@ -0,0 +1,39 @@
+// unitsData.js
+
+export function normalize(type, value, unit) {
+
+  const factors = {
+
+    density: {
+      kgm3: 1,
+      lbft3: 16.0185
+    },
+
+    velocity: {
+      ms: 1,
+      fts: 0.3048
+    },
+
+    length: {
+      m: 1,
+      mm: 0.001,
+      ft: 0.3048,
+      in: 0.0254
+    },
+
+    dynamicViscosity: {
+      Pas: 1,
+      cP: 0.001,
+      lbfts: 1.48816
+    },
+
+    kinematicViscosity: {
+      m2s: 1,
+      cSt: 1e-6,
+      ft2s: 0.092903
+    }
+
+  };
+
+  return value * factors[type][unit];
+}
