9ed30dab342ab5acd112ca08c4930507832bc4b5
ldaeasytech <lda.easytech@gmail.com>
2026-02-22 21:02:45 +0800

    Update converterEngine.js

--- docs/unit-converter/converterEngine.js
+++ docs/unit-converter/converterEngine.js
@@ -1,4 +1,5 @@
 import { units } from "./unitsData.js";
+import { sameDimension } from "./dimensionEngine.js";
 
 export function convert(quantity, value, fromUnit, toUnit) {
 
@@ -7,34 +8,22 @@ export function convert(quantity, value, fromUnit, toUnit) {
   }
 
   const category = units[quantity];
+
   const from = category.units[fromUnit];
   const to = category.units[toUnit];
 
-  const baseValue = value * from.toBase;
-  const result = baseValue / to.toBase;
+  if (!sameDimension(from.dim, to.dim)) {
+    throw new Error("Incompatible unit dimensions.");
+  }
+
+  const baseValue = value * from.factor;
+  const result = baseValue / to.factor;
 
   return {
-    baseValue,
     result,
-    baseUnit: category.base,
-    fromFactor: from.toBase,
-    toFactor: to.toBase
+    baseValue,
+    from,
+    to,
+    quantity
   };
-}
-
-function convertTemperature(value, from, to) {
-
-  let K;
-
-  if (from === "C") K = value + 273.15;
-  else if (from === "F") K = (value - 32) * 5/9 + 273.15;
-  else K = value;
-
-  let result;
-
-  if (to === "C") result = K - 273.15;
-  else if (to === "F") result = (K - 273.15) * 9/5 + 32;
-  else result = K;
-
-  return { result };
 }
