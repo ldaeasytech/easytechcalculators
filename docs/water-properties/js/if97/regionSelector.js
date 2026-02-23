b7a60a6720e0002b0b6b19ee6a959182f119f4ed
ldaeasytech <lda.easytech@gmail.com>
2026-02-24 05:54:14 +0800

    Update regionSelector.js

--- docs/water-properties/js/if97/regionSelector.js
+++ docs/water-properties/js/if97/regionSelector.js
@@ -28,14 +28,14 @@ export function regionSelector({ T, P, h, s, mode }) {
     const hf = h_f_sat(Ts);
     const hg = h_g_sat(Ts);
 
-   // if (Math.abs(h - hf) < H_TOL) return 4;
-   // if (Math.abs(h - hg) < H_TOL) return 4;
+   if (Math.abs(h - hf) < H_TOL) return 4;
+   if (Math.abs(h - hg) < H_TOL) return 4;
 
-    //if (h < hf) return 1;
+    if (h < hf) return 1;
     if (h > hg) return 2;
 
-   // return 4; // two-phase
-    return 1;
+     return 4; // two-phase
+    
   }
 
   /* ==================================================
@@ -49,15 +49,13 @@ export function regionSelector({ T, P, h, s, mode }) {
     const sf = s_f_sat(Ts);
     const sg = s_g_sat(Ts);
 
-   // if (Math.abs(s - sf) < S_TOL) return 4;
-   // if (Math.abs(s - sg) < S_TOL) return 4;
+   if (Math.abs(s - sf) < S_TOL) return 4;
+   if (Math.abs(s - sg) < S_TOL) return 4;
 
-    //if (s < sf) return 1;
+    if (s < sf) return 1;
     if (s > sg) return 2;
 
-    return 1;
-
-   // return 4;
+   return 4;
   }
 
   /* ==================================================
