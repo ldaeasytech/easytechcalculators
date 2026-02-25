8cb95d55f06247aec067e159dcabe4cdd03995a6
ldaeasytech <lda.easytech@gmail.com>
2026-02-25 16:57:04 +0800

    Add files via upload

---
+++ docs/npsh-available-calculator/js/data/pipeRoughness.js
@@ -0,0 +1,12 @@
+// js/data/pipeRoughness.js
+export const PIPE_ROUGHNESS = {
+  "Commercial steel": 4.57e-5,
+  "Drawn tubing": 1.52e-6,
+  "Asphalted cast iron": 1.22e-4,
+  "Galvanized iron": 1.52e-4,
+  "Cast iron": 2.59e-4,
+  "Wood stave (avg)": 5.5e-4,
+  "Concrete (avg)": 1.67e-3,
+  "Riveted steel (avg)": 5.0e-3
+};
+
